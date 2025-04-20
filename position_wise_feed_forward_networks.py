a3679d43a8e18286d4fbf99e72e0ed1b39d05659
Leon Lusbo <leonlusbo11@gmail.com>
2025-04-20 15:17:59 +1000

    Modified position_wise_feed_forward_networks.py

--- position_wise_feed_forward_networks.py
+++ position_wise_feed_forward_networks.py
@@ -1,4 +1,12 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+import math
+import copy
 import multihead_attention
 
 class PositionWiseFeedForward(nn.Module):
-    def __init__(self, )
+    def __init__(self, d_model, d_ff):
+        super(PositionWiseFeedForward, self).__init__()
+        self.fc1 = nn.Linear(d_model, d_ff)
