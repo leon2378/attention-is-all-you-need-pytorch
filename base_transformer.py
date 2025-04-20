be08faa88543c8508d8653abcfe8c4ff1127a126
Leon Lusbo <leonlusbo11@gmail.com>
2025-04-20 13:37:14 +1000

    first commit

---
+++ base_transformer.py
@@ -0,0 +1,31 @@
+# Import the necessary libraries
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+import math
+import copy
+
+
+# Define the Multi-head attention
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_model, num_heads):
+        super(MultiHeadAttention, self).__init__()
+        assert d_model % num_heads == 0   # d_model must be divisible by num_heads
+
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.d_k = d_model // num_heads
+
+        self.W_q = nn.Linear(d_model, d_model)
+        self.W_k = nn.Linear(d_model, d_model)
+        self.W_v = nn.Linear(d_model, d_model)
+        self.W_o = nn.Linear(d_model, d_model)
+
+    def scaled_dot_product_attention(self, Q, K, V, mask=None):
+        attn_scores = torch.matmul(Q, K.transpose(-2, -1)) / math.sqrt(self.d_k)
+
+        if mask is not None:
+            attn_scores = attn_scores.masked_fill(mask == 0, -1e9)
