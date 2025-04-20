f59bb919bb6c98e1ee281b7c0c58f00bd63a6c06
Leon Lusbo <leonlusbo11@gmail.com>
2025-04-20 15:20:34 +1000

    Modified position_wise_feed_forward_networks.py

--- position_wise_feed_forward_networks.py
+++ position_wise_feed_forward_networks.py
@@ -13,4 +13,5 @@ class PositionWiseFeedForward(nn.Module):
         self.fc2 = nn.Linear(d_ff, d_model)
         self.relu = nn.ReLU()
 
-        
+    def forward(self, x):
+        return self.fc2(self.relu(self.fc1(x)))
