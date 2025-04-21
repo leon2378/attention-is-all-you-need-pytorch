a6017a48465ed087795f97bb34b8580e0ae870ef
Leon Lusbo <leonlusbo11@gmail.com>
2025-04-21 22:58:47 +1000

    Modified encoder_layer.py

--- encoder_layer.py
+++ encoder_layer.py
@@ -4,7 +4,11 @@ import torch.optim as optim
 import torch.utils.data as data
 import math
 import copy
+
+import multihead_attention
 from multihead_attention import MultiHeadAttention
+
+import position_wise_feed_forward_networks
 from position_wise_feed_forward_networks import PositionWiseFeedForward
 #from positional_encoding import PositionalEncoding
 
