c40cc552de5ec9192f150059e21d609b9b1e1fc1
Leon Lusbo <leonlusbo11@gmail.com>
2025-04-21 23:04:58 +1000

    Modified decoder_layer.py

--- decoder_layer.py
+++ decoder_layer.py
@@ -1,1 +1,13 @@
 import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+import math
+import copy
+
+import multihead_attention
+from multihead_attention import MultiHeadAttention
+
+import position_wise_feed_forward_networks
+from position_wise_feed_forward_networks import PositionWiseFeedForward
+
