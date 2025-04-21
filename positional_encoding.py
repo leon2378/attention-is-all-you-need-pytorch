2cba3a1bdcbe483c6cedc5751559728b86faacaf
Leon Lusbo <leonlusbo11@gmail.com>
2025-04-21 14:08:20 +1000

    Modified positional_encoding.py

--- positional_encoding.py
+++ positional_encoding.py
@@ -1,1 +1,15 @@
 import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+import math
+import copy
+import multihead_attention
+import position_wise_feed_forward_networks
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, max_seq_length):
+        super(PositionalEncoding, self).__init__()
+
+        
