adb28b53b4c24726d6d34d5f0f676014a4d41e07
Leon Lusbo <leonlusbo11@gmail.com>
2025-04-21 14:51:06 +1000

    Modified encoder_layer.py

--- encoder_layer.py
+++ encoder_layer.py
@@ -4,8 +4,18 @@ import torch.optim as optim
 import torch.utils.data as data
 import math
 import copy
-import multihead_attention, position_wise_feed_forward_networks, positional_encoding
+from multihead_attention import MultiHeadAttention
+from position_wise_feed_forward_networks import PositionWiseFeedForward
+from positional_encoding import PositionalEncoding
 
 class EncoderLayer(nn.Module):
     def __init__(self, d_model, num_heads, d_ff, dropout):
         super(EncoderLayer, self).__init__()
+
+        self.attn = MultiHeadAttention(d_model, num_heads)
+        self.feed_forward = PositionWiseFeedForward(d_model, d_ff)
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    
