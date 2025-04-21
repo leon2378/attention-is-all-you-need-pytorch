310310011f29a75be683884efafdf37f756eb83a
Leon Lusbo <leonlusbo11@gmail.com>
2025-04-21 23:26:03 +1000

    Added transformer.py

--- transformer.py
+++ transformer.py
@@ -8,3 +8,8 @@ import copy
 import positional_encoding
 from positional_encoding import PositionalEncoding
 
+class Transformer(nn.Module):
+    def __init__(self, src_vocab_size, tgt_vocab_size, d_model, num_heads, num_layers, d_ff, max_seq_length, dropout):
+        super(Transformer, self).__init__()
+
+        
