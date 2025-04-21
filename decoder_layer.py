cd4901430870643050314c91c209031fda4dad41
Leon Lusbo <leonlusbo11@gmail.com>
2025-04-21 23:15:12 +1000

    Modified decoder_layer.py

--- decoder_layer.py
+++ decoder_layer.py
@@ -11,3 +11,18 @@ from multihead_attention import MultiHeadAttention
 import position_wise_feed_forward_networks
 from position_wise_feed_forward_networks import PositionWiseFeedForward
 
+class DecoderLayer(nn.Module):
+    def __init__(self, d_model, num_heads, d_ff, dropout):
+        super(DecoderLayer, self).__init__()
+
+        self.self_attn = MultiHeadAttention(d_model, num_heads)
+        self.cross_attn = MultiHeadAttention(d_model, num_heads)
+        self.feed_forward = PositionWiseFeedForward(d_model, d_ff)
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.norm3 = nn.LayerNorm(d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, enc_output, src_mask, tgt_mask):
+        attn_output = self.self_attn(x, x, x, tgt_mask)
+        x = self.norm1(x + self.dropout(attn_output))
