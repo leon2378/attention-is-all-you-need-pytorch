dce5701900a70eb49c8831db1ea15a511bf2c396
Leon Lusbo <leonlusbo11@gmail.com>
2025-04-21 14:37:27 +1000

    Modified encoder_layer.py

--- encoder_layer.py
+++ encoder_layer.py
@@ -6,3 +6,6 @@ import math
 import copy
 import multihead_attention, position_wise_feed_forward_networks, positional_encoding
 
+class EncoderLayer(nn.Module):
+    def __init__(self, d_model, num_heads, d_ff, dropout):
+        super(EncoderLayer, self).__init__()
