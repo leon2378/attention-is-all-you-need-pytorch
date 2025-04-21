6d214ae202a2be5c8036ac8effe46166c5e4c048
Leon Lusbo <leonlusbo11@gmail.com>
2025-04-21 14:29:37 +1000

    Modified positional_encoding.py

--- positional_encoding.py
+++ positional_encoding.py
@@ -16,5 +16,10 @@ class PositionalEncoding(nn.Module):
         position = torch.arange(0, max_seq_length, dtype=torch.float).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model))
 
-        pe[:, 0::2]
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
 
+        self.register_buffer('pe', pe.unsqueeze(0))
+
+    def forward(self, x):
+        return x + self.pe[:, :x.size(1)]
