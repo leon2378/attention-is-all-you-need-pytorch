660c7c78e47b34686757f247a71e62d928661b41
Leon Lusbo <leonlusbo11@gmail.com>
2025-04-21 23:23:57 +1000

    Added transformer.py

---
+++ transformer.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+import math
+import copy
+
+import positional_encoding
+from positional_encoding import PositionalEncoding
+
