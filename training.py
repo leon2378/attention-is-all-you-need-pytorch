e5c89ac06d5dcf45de76647e30910586aec83d21
Leon Lusbo <leonlusbo11@gmail.com>
2025-07-07 01:26:58 +1000

    Added training.py

---
+++ training.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import transformer
+import sample_data
+from sample_data import src_data, tgt_data, tgt_vocab_size
+criterion = nn.CrossEntropyLoss(ignore_index=0)
+optimizer = optim.Adam(transformer.parameters(), lr=0.0001, betas=(0.9, 0.98), eps=1e-9)
+
+transformer.train()
+
+for epoch in range(100):
+    optimizer.zero_grad()
+    output = transformer(src_data, tgt_data[:, :-1])
+    loss = criterion(output.contiguous().view(-1, tgt_vocab_size), tgt_data[:, 1:].contiguous().view(-1))
+    loss.backward()
+    optimizer.step()
+    print(f"Epoch: {epoch+1}, Loss: {loss.item()}")
